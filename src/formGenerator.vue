ce09dd5ce37de905b09d99cf1b39a39bdfde4ad8
Laura <bloren93@gmail.com>
2020-03-30 19:42:20 +0200

    fix legend2

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -6,7 +6,8 @@ div.vue-form-generator(v-if='schema != null')
 
 	template(v-for='group in groups')
 		fieldset(:is='tag', :class='getFieldRowClasses(group)')
-			legend(v-if='group.legend', v-html='group.legend')
+			legend(v-if='group.legend')
+				span(v-html='group.legend')
 			template(v-for='field in group.fields')
 				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
 </template>
